3044d57387aed924fdfc735370845f79d2912c8c
Brendan Higgins <bjh83@case.edu>
2014-02-28 04:38:49 -0500

    I tried to improve point comparisons.

--- MatchFeatures.m
+++ MatchFeatures.m
@@ -1,18 +1,38 @@
-clear all
-close all
-GenerateDescriptors
-pause;
+% clear all
+% close all
+% GenerateDescriptors
+% pause;
 
 pc = [];
+H = Haar(8);
 for c_1 = 1 : 10
     for c_2 = 1 : 10
         for c_3 = 1 : 10
             for n = 1 : 20
                 i_1 = LookupTable_1(c_1, c_2, c_3, n, 1);
                 j_1 = LookupTable_1(c_1, c_2, c_3, n, 2);
-                i_2 = LookupTable_2(c_1, c_2, c_3, n, 1);
-                j_2 = LookupTable_2(c_1, c_2, c_3, n, 2);
-                if i_1 ~= 0 && i_2 ~= 0
+                if i_1 ~= 0 && LookupTable_2(c_1, c_2, c_3, 1, 1) ~= 0
+                    d_1 = I_1((i_1 - 3) : (i_1 + 4), (j_1 - 3) : (j_1 + 4));
+                    m = sum(sum(d_1)) / 8^2;
+                    v = sqrt(sum(sum((d_1 - m).^2)) / 8^2);
+                    d_1 = (d_1 - m) / v;
+                    d_1 = H*d_1*H';
+                    dist = zeros(1, 20) + inf;
+                    for k = 1 : 20
+                        i_2 = LookupTable_2(c_1, c_2, c_3, k, 1);
+                        j_2 = LookupTable_2(c_1, c_2, c_3, k, 2);
+                        if i_2 ~= 0
+                            d_2 = I_2((i_2 - 3) : (i_2 + 4), (j_2 - 3) : (j_2 + 4));
+                            m = sum(sum(d_2)) / 8^2;
+                            v = sqrt(sum(sum((d_2 - m).^2)) / 8^2);
+                            d_2 = (d_2 - m) / v;
+                            d_2 = H*d_2*H';
+                            dist(k) = sum(sum(abs(d_1 - d_2)));
+                        end
+                    end
+                    [~, k] = min(dist);
+                    i_2 = LookupTable_1(c_1, c_2, c_3, k, 1);
+                    j_2 = LookupTable_1(c_1, c_2, c_3, k, 2);
                     pc = [pc; i_1, j_1, i_2, j_2];
                 end
             end
